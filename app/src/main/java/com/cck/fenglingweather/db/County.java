0a880e551545b0d724512ca4abca729454e7e40a
cck <994470425@qq.com>
2017-03-09 22:33:41 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/cck/fenglingweather/db/County.java
@@ -0,0 +1,48 @@
+package com.cck.fenglingweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/3/9.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+
+}
